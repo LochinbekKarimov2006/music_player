f96d774f10ea287febfc5183d77fe1d9b1f47b46
LochinbekKarimov2006 <161306293+LochinbekKarimov2006@users.noreply.github.com>
2024-10-18 17:16:43 +0500

    .

--- src/utils/Token.js
+++ src/utils/Token.js
@@ -1,6 +1,6 @@
 let CLIENT_ID = "bccf8c1a5c48453287736a546c4c48d5";
 let CLIENT_SECRET = "4b3db2d586414d4287e00691aeaf7ce1";
-const getToken = async () => {
+ const getToken = async () => {
   try {
     const response = await fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
